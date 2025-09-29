535e0209920cc6a41d0dd459d0049d0b16c160e6
meowrzboy <rommaosipov@gmail.com>
2025-09-29 18:52:25 +0300

    v3, начал делать логику обработки хода игрока

--- src/components/ChooseMoveBlock.jsx
+++ src/components/ChooseMoveBlock.jsx
@@ -2,16 +2,20 @@ import Button from "./Button.jsx";
 
 import "../App.css";
 
-export default function ChooseMoveBlock() {
+export default function ChooseMoveBlock({
+  choseRock,
+  chosePaper,
+  choseScissors,
+}) {
   return (
     <div className="flex flex-col gap-[24px]">
       <div className="font-display text-[14px] font-medium text-[#353535] text-center">
         Choose your move, rock paper or scissors?
       </div>
       <div className="flex gap-[48px]">
-        <Button>ROCK</Button>
-        <Button>PAPER</Button>
-        <Button>SCISSORS</Button>
+        <Button onClick={choseRock}>ROCK</Button>
+        <Button onClick={chosePaper}>PAPER</Button>
+        <Button onClick={choseScissors}>SCISSORS</Button>
       </div>
     </div>
   );
--- src/components/GameplayBlock.jsx
+++ src/components/GameplayBlock.jsx
@@ -1,11 +1,22 @@
+// import { useState } from "react";
 import fist_icon from "../assets/fist_icon.svg";
+
+// import paper_icon from "../assets/paper_icon.svg";
+// import scissors_icon from "../assets/scissors_icon.svg";
 
 import "../App.css";
 
 export default function GameplayBlock() {
+  // const [newMove, setNewMove] = useState({ fist });
+
+  // const PlayerMove = (props) => {
+  //   setNewMove(props);
+  // };
+
   return (
     <div className="flex gap-[58px] justify-center">
       <img src={fist_icon} className="scale-x-[-1] w-[100px] h-[100px]" />
+
       <div className="text-[20px] font-bold font-display text-black mt-[50px] mb-[26px]">
         VS
       </div>
